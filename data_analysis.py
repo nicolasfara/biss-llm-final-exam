5b4614784b78ec64b2d873a0f03e51fe3a393af4
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-06-26 18:38:50 +0200

    new charts

--- data_analysis.py
+++ data_analysis.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
         "https://raw.githubusercontent.com/nicolasfara/biss-llm-final-exam/master/data/acti-a/subtaskA_train.csv"
     )
     test_dataset = retrieve_data_as_dataframe(
-        "https://raw.githubusercontent.com/nicolasfara/biss-llm-final-exam/master/data/acti-a/subtaskA_test_with_labels.csv"
+        "https://raw.githubusercontent.com/nicolasfara/biss-llm-final-exam/master/data/acti-a/subtaskA_test.csv"
     )
     # Compare side by side the distribution of the classes of the dataset and the test dataset
-    _, axes = plt.subplots(1, 2, figsize=(20, 3), layout='tight')
+    _, axes = plt.subplots(1, 2, figsize=(20, 5), layout='tight')
 
     sns.countplot(x='conspiratorial', data=dataset, ax=axes[0], hue='conspiratorial', legend=False)
     axes[0].set_title("Train Dataset Class Distribution", fontsize=16)
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 
     # plot the distribution length of the comments using the columns "comment_text" for dataset and test_dataset
 
-    _, axes = plt.subplots(2, 2, figsize=(20, 5), layout='tight', sharex=True)
+    _, axes = plt.subplots(2, 2, figsize=(20, 7), layout='tight', sharex=True)
     dataset['comment_text_length'] = dataset['comment_text'].apply(lambda x: len(x))
     sns.histplot(dataset['comment_text_length'], kde=True, ax=axes[0, 0])
     axes[0, 0].set_title("Train Dataset Comment Length Distribution", fontsize=16)
@@ -53,5 +53,11 @@ if __name__ == '__main__':
     axes[1, 1].set_xlabel("Comment Length (characters)", fontsize=14)
     axes[1, 1].set_ylabel("Frequency", fontsize=14)
     plt.savefig("charts/comment_length_distribution.pdf")
+
+    # provide statistics for the train and test dataset
+    print("Train Dataset Statistics")
+    print(dataset.describe())
+    print("Test Dataset Statistics")
+    print(test_dataset.describe())
 
     plt.show()
